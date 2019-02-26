fc38189a8f44ac28bd3d589442556b24f9064590
Linh Dan Nguyen <lhn222@nyu.edu>
2019-02-26 21:54:21 +0000

    Automatically backed up by Learn

--- more_js/example_two.js
+++ more_js/example_two.js
@@ -1,3 +1,3 @@
 for ( let n = 0; n < 10; n++ ) {
-  console.log( n );
+  console.log( n );https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/programming-js-in-the-ide/from-dom-to-node-and-the-ide#
 }
